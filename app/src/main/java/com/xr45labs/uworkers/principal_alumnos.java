12f8ff26728a96e08996913c79eea5d792293599
xr45 <carlos_app08@hotmail.com>
2017-03-05 19:40:04 -0600

    Diseno de el fragment de perfil del alumno y empresa ,falta  el cargado de archivo y funcionalidad de modificaciones

--- app/src/main/java/com/xr45labs/uworkers/principal_alumnos.java
+++ app/src/main/java/com/xr45labs/uworkers/principal_alumnos.java
@@ -4,26 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mikepenz.iconics.context.IconicsContextWrapper;
 import com.xr45labs.uworkers.fragments.fr_bvacantes;
-import com.xr45labs.uworkers.fragments.fr_perfil;
+import com.xr45labs.uworkers.fragments.fr_perfil_alumno;
 
 public class principal_alumnos extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
-        fr_perfil.OnFragmentInteractionListener,
+        fr_perfil_alumno.OnFragmentInteractionListener,
         fr_bvacantes.OnFragmentInteractionListener{
 
     Fragment fragment = null;
@@ -67,7 +63,7 @@ public class principal_alumnos extends AppCompatActivity
             fragment = new fr_bvacantes();
             FragmentTransaction = true;
         } else if (id == R.id.nav_perfil) {
-            fragment = new  fr_perfil();
+            fragment = new fr_perfil_alumno();
             FragmentTransaction = true;
         }  else if (id == R.id.nav_logout) {
             Intent intent = new Intent(this, MainActivity.class);
--- app/src/main/java/com/xr45labs/uworkers/principal_empresa.java
+++ app/src/main/java/com/xr45labs/uworkers/principal_empresa.java
@@ -4,17 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mikepenz.iconics.context.IconicsContextWrapper;
@@ -22,7 +18,7 @@ import com.xr45labs.uworkers.fragments.*;
 
 public class principal_empresa extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
-        fr_perfil.OnFragmentInteractionListener,
+        fr_perfil_empresa.OnFragmentInteractionListener,
         fr_bvacantes.OnFragmentInteractionListener,
         fr_balumnos.OnFragmentInteractionListener{
 
@@ -73,7 +69,7 @@ public class principal_empresa extends AppCompatActivity
             FragmentTransaction = true;
 
         } else if (id == R.id.nav_perfil) {
-            fragment = new fr_perfil();
+            fragment = new fr_perfil_empresa();
             FragmentTransaction = true;
 
         } else if (id == R.id.nav_logout) {
