0558a8522cd5e993c94bafb897f7dfccf5989d60
xr45 <carlos_app08@hotmail.com>
2017-03-16 19:46:50 -0600

    avances de funcionalidad de pantallas de insituto, creacion de algunos fragments, se crearan fragments para mostrar la vacante seleccionada, para modificar vacantes, muestra de perfil sin modificacion para la visualizacion de otros tipos de usuarios, se modifico el login, se agregaron mas fragments y se reorganizaron fragments creados por tipo de usuario

--- app/src/main/java/com/xr45labs/uworkers/crearcuenta_alumno_content.java
+++ app/src/main/java/com/xr45labs/uworkers/crearcuenta_alumno_content.java
@@ -1,13 +1,38 @@
 package com.xr45labs.uworkers;
 
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class crearcuenta_alumno_content extends AppCompatActivity {
+
+import com.mikepenz.iconics.context.IconicsContextWrapper;
+import com.xr45labs.uworkers.fragments.alumnos.fr_registro_alumnos;
+
+public class crearcuenta_alumno_content extends AppCompatActivity implements fr_registro_alumnos.OnFragmentInteractionListener {
+
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crearcuenta_alumno_content);
+
+        fragment = new fr_registro_alumnos();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.activiy_crearcuenta_alumno_content,fragment).commit();
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(newBase));
     }
 }
--- app/src/main/java/com/xr45labs/uworkers/crearcuenta_empresa_content.java
+++ app/src/main/java/com/xr45labs/uworkers/crearcuenta_empresa_content.java
@@ -1,13 +1,34 @@
 package com.xr45labs.uworkers;
 
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class crearcuenta_empresa_content extends AppCompatActivity {
+import com.mikepenz.iconics.context.IconicsContextWrapper;
+import com.xr45labs.uworkers.fragments.empresas.fr_registro_empresa;
 
+public class crearcuenta_empresa_content extends AppCompatActivity implements fr_registro_empresa.OnFragmentInteractionListener {
+
+    Fragment fragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crearcuenta_empresa_content);
+
+        fragment = new fr_registro_empresa();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.activity_crearcuenta_empresa_content,fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(newBase));
     }
 }
